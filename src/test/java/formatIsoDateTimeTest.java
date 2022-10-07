434f292ab6a475f23872f356055793241aa5a96c
JoaoASousa <joaosousa.pt@gmail.com>
2022-10-07 22:16:50 +0100

    Added/fixed tests

--- src/test/java/formatIsoDateTimeTest.java
+++ src/test/java/formatIsoDateTimeTest.java
@@ -21,21 +21,31 @@ public class formatIsoDateTimeTest {
 
     @Test
     public void testNullFormatter() {
-        Assertions.assertThrows(NullPointerException.class,
-                () -> formatIsoDateTime("2020-10-09", null)
-        );
+
+        String output = formatIsoDateTime("2020-10-09", null);
+        String expected = formatIsoDateTime("2020-10-09", DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+
+        Assertions.assertEquals(expected, output);
+    }
+
+    @Test
+    public void testDateTimeParseException() {
+        DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_DATE;
+        String input = "2018-07-14";
+
+        String output = formatIsoDateTime(input, formatter);
+
+        Assertions.assertEquals("1970-01-01", output);
     }
 
     @Test
     public void testValidArguments() {
         DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_DATE;
-        String input = LocalDateTime.now().format(formatter);
+        String input = "2018-07-14T17:45:55.9483536";
 
-        String expected = formatIsoDateTime(input, formatter);
-        String output = formatIsoDateTime(null, formatter);
+        String output = formatIsoDateTime(input, formatter);
 
-        Assertions.assertEquals(expected, output);
+        Assertions.assertEquals("2018-07-14", output);
     }
 
-    // excption para o try catch
 }
