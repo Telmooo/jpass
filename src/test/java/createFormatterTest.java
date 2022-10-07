4cb9c0b7ac398c8f5437c8ee74b2c21c8308f33e
JoaoASousa <joaosousa.pt@gmail.com>
2022-10-07 21:36:56 +0100

    Added another test case / some refactoring

--- src/test/java/createFormatterTest.java
+++ src/test/java/createFormatterTest.java
@@ -1,41 +1,64 @@
 import static jpass.util.DateUtils.createFormatter;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.temporal.ChronoUnit;
 
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class createFormatterTest {
 
     @Test
-    public void testingNull() {
+    public void testNullPointer() {
 
         DateTimeFormatter dtf = createFormatter(null);
 
-        Assert.assertTrue(dtf.getClass() == DateTimeFormatter.class);
-        Assert.assertEquals(DateTimeFormatter.ISO_DATE, dtf);
+        Assertions.assertEquals(DateTimeFormatter.ISO_DATE, dtf);
     }
 
     @Test
-    public void testingEmpty() {
-        String input = "";
+    public void testIllegalArgument() {
+        DateTimeFormatter dtf = createFormatter("aaaaaa");
+
+        Assertions.assertEquals(DateTimeFormatter.ISO_DATE, dtf);
+    }
+
+    @Test
+    public void testEmpty() {
+        LocalDateTime l = LocalDateTime.now();
+
+        DateTimeFormatter dtf = createFormatter("");
+        String expectedStr = l.format(new DateTimeFormatterBuilder().toFormatter());
+        String outputStr = l.format(dtf);
+
+        Assertions.assertEquals(expectedStr, outputStr);
+    }
+
+    @Test
+    public void testValidDate() {
+        String input = "yyyyMMdd";
+        LocalDate l = LocalDate.now();
 
         DateTimeFormatter dtf = createFormatter(input);
+        String expectedStr = l.format(DateTimeFormatter.BASIC_ISO_DATE);
+        String outputStr = l.format(dtf);
 
-        Assert.assertTrue(dtf.getClass() == DateTimeFormatter.class);
+        Assertions.assertEquals(expectedStr, outputStr);
     }
 
     @Test
-    public void testingConstants() {
-        String[] arr = {
-                "yyyy MM dd",
-        };
+    public void testValidTime() {
+        String input = "HH:mm:ss";
+        LocalTime l = LocalTime.now().truncatedTo(ChronoUnit.SECONDS);
 
+        DateTimeFormatter dtf = createFormatter(input);
+        String expectedStr = l.format(DateTimeFormatter.ISO_LOCAL_TIME);
+        String outputStr = l.format(dtf);
 
-        for (String format : arr) {
-            DateTimeFormatter dtf = createFormatter(format);
-
-
-        }
+        Assertions.assertEquals(expectedStr, outputStr);
     }
 }
